8969420be74fc77df7bdb65ed64c5530474b037e
JOJEONGMIN <mnj190>
2020-08-26 17:10:49 +0900

    0826

--- maintest/src/main/webapp/WEB-INF/resources/js/mypage/listView.js
+++ maintest/src/main/webapp/WEB-INF/resources/js/mypage/listView.js
@@ -49,17 +49,26 @@
 								console.log('변경됨')
 							}
 						}
-
-						var g_oBeforeBody = document
-								.getElementById('container').innerHTML;
-
-						$('#printbtn').click(function() {
-
-							// 프린트를 보이는 그대로 나오기위한 셋팅
-							window.onbeforeprint = function(ev) {
-								document.body.innerHTML = g_oBeforeBody;
-							};
+		
+						$('#printbtn').click(function() {							
 							window.print();
-							location.reload();
 						});
 					});
+					
+					
+  $('#pdfbtn').click(function () {
+
+    var x = document.querySelector('html');
+    x.style.width = '800px';
+
+    //pdf_wrap을 canvas객체로 변환
+    html2canvas($('.page')[0]).then(function (canvas) {
+      var doc = new jsPDF('p', 'mm', 'a4'); //jspdf객체 생성
+      var imgData = canvas.toDataURL('image/png'); //캔버스를 이미지로 변환
+      doc.addImage(imgData, 'PNG', 0, 0); //이미지를 기반으로 pdf생성
+      doc.save('mylist.pdf'); //pdf저장
+    });    
+    
+    x.style.width = 'auto';
+    
+  });
