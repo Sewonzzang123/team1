eb384740d102e801df48aa7c793992b054778a0e
JOJEONGMIN <mnj190>
2020-09-18 15:14:10 +0900

    수정

--- maintest/src/main/java/com/my/maintest/board/dao/PagingDAO.java
+++
@@ -1,9 +0,0 @@
-package com.my.maintest.board.dao;
-
-public interface PagingDAO {
-
-
-//페이징
-		//레코드 총 수량 구하기 
-	int selectRecQnty(String searchType, String searchKeyword);
-}
--- maintest/src/main/java/com/my/maintest/board/vo/BoardVO.java
+++ maintest/src/main/java/com/my/maintest/board/vo/BoardVO.java
@@ -10,44 +10,41 @@ import javax.validation.constraints.Size;
 
 import org.springframework.web.multipart.MultipartFile;
 
-import com.sun.istack.Nullable;
-
 import lombok.Data;
 
-@Entity
 @Data
+@Entity
 public class BoardVO {
-
 	private long bnum; // bnum number primary key, --게시글 번호
-//	@Valid
+	@Valid
 	private BcategoryVO bcategory; // catNum catName //bcategory number , -- 게시판 코드
-//	@Valid
+	@Valid
 	private HeadIdCategoryVO hidcategory; // hidNum catNum hidName //hidnum number, --말머리 번호
 
 	private long ucode; // ucode number NOT NULL, --사용자 코드 아이디
-//	@NotNull
+	@NotNull
 	@Size(min = 2, max = 20, message = "제목은최소 2자,  최대 20자까지 입력가능합니다.")
 	private String btitle; // btitle varchar2(60) NOT NULL, --게시글 제목
 //	@NotNull
-	@Size(min = 4, max = 1500, message = "본문은 최소 4자,  최대 1500자까지 입력가능합니다.")
-	private String bcontent; // bcontent varchar2(500) NOT NULL, -- 게시글 내용
-	private String tcontent; // bcontent varchar2(500) NOT NULL, -- 게시글 내용
+//	@Size(min = 4, max = 1500, message = "본문은 최소 4자,  최대 1500자까지 입력가능합니다.")
+	private byte[] bcontent; // bcontent varchar2(500) NOT NULL, -- 게시글 내용
+	private String tcontent;
 	private long bhits; // bhits number NOT NULL, --조회수
 	private Timestamp bcdate; // bCdate timestamp NOT NULL, --생성일
 	private Timestamp budate; // bUdate timestamp NOT NULL, --수정일
 	private long bgroup; // bGroup number (5), --답글 그룹번호
 	private long bstep; // bstep number(5), --답글 순서
 	private long bindent; // bindent number(5), --답글 들여쓰기
-	private byte[] thumbnail; // 썸네일
-	private int num; // 아래 dbrownum과 용도가 같은듯????
 
 	private long dbrownum; // SQL : board "dbrownum" 게시글 목록 row_number() over(order by bgroup asc, bstep
 							// desc) as dbrownum로 생성
 	private String bnickname; // SQL : member.nickname as "bnickname"
+	private byte[] thumbnail;
 
 //첨부파일
 
 	private List<MultipartFile> files;
 	private BoardFileVO boardFileVO;
+	private ThumbnailVO thumbnailVO; // 섬네일
 
 }
